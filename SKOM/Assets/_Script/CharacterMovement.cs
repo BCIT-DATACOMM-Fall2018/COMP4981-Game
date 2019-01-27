59cc3b500d88def4d29978a3976fb6c4f6715b6d
Jason Kim <jjkim2320@gmail.com>
2019-01-26 21:11:32 -0800

    beta mouseMovement using NavMesh AI unity

---
+++ SKOM/Assets/_Script/CharacterMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterMovement : MonoBehaviour
+{
+    NavMeshAgent agent;
+    private Vector2 mouseOver;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if(!Camera.main)
+        {
+            Debug.Log("No Main camera");
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Debug.Log(Input.mousePosition);
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100, LayerMask.GetMask("Field")))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
