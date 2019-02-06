03598f854bb8f31e48b052045934db309f3a322b
Dasha Strigoun <dstrigoun@outlook.com>
2019-02-06 13:11:33 -0800

    Add function headers

--- SKOM/Assets/_Script/ClientWrapper.cs
+++ SKOM/Assets/_Script/ClientWrapper.cs
@@ -27,18 +27,88 @@ public class ClientWrapper
     private int _Id;
     private int _Health;
     
+    /// ---------------------------------------------- 
+    /// FUNCTION:    Set Id of character
+    ///
+    /// DATE:        February 6th, 2019
+    ///        
+    /// REVISION:    
+    ///
+    /// DESIGNER:    Dasha Strigoun
+    ///
+    /// PROGRAMMER:  Dasha Strigoun
+    ///
+    /// INTERFACE:   SetId(int id)
+    ///                 - int id: new id
+    ///
+    /// RETURNS:     void
+    ///
+    /// NOTES:       
+    /// ---------------------------------------------- 
     public void SetId(int id) {
         _Id = id;
     }
 
+    /// ---------------------------------------------- 
+    /// FUNCTION:    Get Id of character
+    ///
+    /// DATE:        February 6th, 2019
+    ///        
+    /// REVISION:    
+    ///
+    /// DESIGNER:    Dasha Strigoun
+    ///
+    /// PROGRAMMER:  Dasha Strigoun
+    ///
+    /// INTERFACE:   GetId()
+    ///
+    /// RETURNS:     int
+    ///
+    /// NOTES:       
+    /// ---------------------------------------------- 
     public int GetId() {
         return _Id;
     }
 
+    /// ---------------------------------------------- 
+    /// FUNCTION:    Set Health of character
+    ///
+    /// DATE:        February 6th, 2019
+    ///        
+    /// REVISION:    
+    ///
+    /// DESIGNER:    Dasha Strigoun
+    ///
+    /// PROGRAMMER:  Dasha Strigoun
+    ///
+    /// INTERFACE:   SetHealth(int health)
+    ///                 - int health: new health
+    ///
+    /// RETURNS:     void
+    ///
+    /// NOTES:       
+    /// ---------------------------------------------- 
     public void SetHealth(int health) {
         _Health = health;
     }
 
+    /// ---------------------------------------------- 
+    /// FUNCTION:    Get Health of character
+    ///
+    /// DATE:        February 6th, 2019
+    ///        
+    /// REVISION:    
+    ///
+    /// DESIGNER:    Dasha Strigoun
+    ///
+    /// PROGRAMMER:  Dasha Strigoun
+    ///
+    /// INTERFACE:   GetHealth()
+    ///
+    /// RETURNS:     int
+    ///
+    /// NOTES:       
+    /// ---------------------------------------------- 
     public int GetHealth() {
         return _Health;
     }
