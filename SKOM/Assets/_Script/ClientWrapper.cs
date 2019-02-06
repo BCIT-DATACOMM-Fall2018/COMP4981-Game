8e2e955c8d3ef45de14b33a7424d741e37b7321a
Dasha Strigoun <dstrigoun@outlook.com>
2019-02-06 13:02:21 -0800

    Created ClientWrapper and added set/get functions for id and health

---
+++ SKOM/Assets/_Script/ClientWrapper.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// ---------------------------------------------- 
+/// Class:        ClientWrapper
+///
+/// PROGRAM:      Some Kind Of MOBA
+///
+/// FUNCTIONS:    public void SetId(int id)
+///               public int GetId()
+///               public void SetHealth(int health)
+///               public int GetHealth()
+/// 
+/// DATE:         February 6th, 2019
+///
+/// REVISIONS:    
+///
+/// DESIGNER:     Dasha Strigoun
+///
+/// PROGRAMMER:   Dasha Strigoun
+///
+/// NOTES:        
+/// ---------------------------------------------- 
+public class ClientWrapper
+{
+    private int _Id;
+    private int _Health;
+    
+    public void SetId(int id) {
+        _Id = id;
+    }
+
+    public int GetId() {
+        return _Id;
+    }
+
+    public void SetHealth(int health) {
+        _Health = health;
+    }
+
+    public int GetHealth() {
+        return _Health;
+    }
+}
