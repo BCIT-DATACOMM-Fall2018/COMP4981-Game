d2834dde93555cb4f06bbf285e96f71c7517d7c2
Keishi Asai <isheik808@gmail.com>
2019-02-07 04:27:08 -0800

    Add files for health stat and its logic

---
+++ SKOM/Assets/_Script/Character.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Status
+{
+    public float hp;
+}
+
+public class Character : MonoBehaviour
+{
+    private CharacterUI cUI;
+    private Status status;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cUI = GetComponent<CharacterUI>();
+        initStatus();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cUI.updateCharacterUI(status);
+        subtractHP(0.5f);
+    }
+    
+    private void initStatus()
+    {
+        status.hp = 60;
+    }
+
+    private void subtractHP(float damage)
+    {
+        // TODO: should call client wrapper?
+        status.hp -= damage;
+    }
+}
---
+++ SKOM/Assets/_Script/CharacterUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterUI : MonoBehaviour
+{
+    Slider hpBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hpBar = GameObject.Find("HPBar").GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void updateCharacterUI(Status status)
+    {
+        hpBar.value = status.hp;
+    }
+}
