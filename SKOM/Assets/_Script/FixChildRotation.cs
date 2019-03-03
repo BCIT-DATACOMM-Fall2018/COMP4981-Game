b6824fbec860e32d65070c007b43997839c7755e
Keishi Asai <isheik808@gmail.com>
2019-03-02 20:13:38 -0800

    Add HPBar

---
+++ SKOM/Assets/_Script/FixChildRotation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// ---------------------------------------------- 
+/// Class:        FixChildRotation
+///
+/// PROGRAM:      Some Kind Of MOBA
+///
+/// FUNCTIONS:    public void Awake()
+///               public void Update()
+/// 
+/// DATE:         March 2nd, 2019
+///
+/// REVISIONS:    
+///
+/// DESIGNER:     Keishi Asai
+///
+/// PROGRAMMER:   Keishi Asai
+///
+/// NOTES:
+/// This script prevents a child object from
+/// rotating together with its parent's object rotation.
+/// ---------------------------------------------- 
+public class FixChildRotation : MonoBehaviour
+{
+    Quaternion rotation;
+    void Awake()
+    {
+        rotation = transform.rotation;
+    }
+    void LateUpdate()
+    {
+        transform.rotation = rotation;
+    }
+}
