68e882b7379d483d256ad18a56d4a797272ffc15
Cameron Roberts <cameronroberts@shaw.ca>
2019-03-16 22:05:30 -0700

    Add an ability bar to display cooldowns

--- Assets/Scripts/Character/PlayerAbilityController.cs
+++ Assets/Scripts/Character/PlayerAbilityController.cs
@@ -1,4 +1,4 @@
-
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -26,7 +26,11 @@ using NetworkLibrary.MessageElements;
 /// ----------------------------------------------
 public class PlayerAbilityController : AbilityController
 {
-    
+    private const float SERVER_TICK_RATE_PER_SECOND = 33f / 1000f;
+    private AbilityType[] abilities;
+    private string[] buttonNames;
+    public float[] Cooldowns {get;set;}
+    public float[] MaxCooldowns {get;set;}
 
     /// ----------------------------------------------
     /// FUNCTION:	Start
@@ -49,6 +53,16 @@ public class PlayerAbilityController : AbilityController
     protected override void Start()
     {
         base.Start();
+
+        abilities = new AbilityType[] {AbilityType.TestProjectile, AbilityType.TestTargeted, AbilityType.TestTargetedHoming, AbilityType.TestAreaOfEffect};
+        buttonNames = new String[] {"Ability1", "Ability2", "Ability3", "Ability4"};
+        Cooldowns = new float[4];
+        MaxCooldowns = new float[4];
+
+        for (int i = 0; i < abilities.Length; i++)
+        {
+            MaxCooldowns[i] = (float)AbilityInfo.InfoArray[(int)abilities[i]].Cooldown * SERVER_TICK_RATE_PER_SECOND;
+        }
     }
 
     /// ----------------------------------------------
@@ -73,18 +87,28 @@ public class PlayerAbilityController : AbilityController
     /// ----------------------------------------------
     void Update()
     {
-        if(Input.GetButtonDown("Ability1")) {
-            InitiateAbilityUse(AbilityType.TestProjectile);
-        } else if (Input.GetButtonDown("Ability2")){
-            InitiateAbilityUse(AbilityType.TestTargeted);
-        } else if (Input.GetButtonDown("Ability3")){
-            InitiateAbilityUse(AbilityType.TestTargetedHoming);
-        } else if (Input.GetButtonDown("Ability4")){
-            InitiateAbilityUse(AbilityType.TestAreaOfEffect);
+        for (int i = 0; i < Cooldowns.Length; i++)
+        {
+            Cooldowns[i] -= Time.deltaTime;
+            if(Cooldowns[i] < 0){
+                Cooldowns[i] = 0;
+            }
+
+        }
+        for (int i = 0; i < buttonNames.Length; i++)
+        {
+            if(Input.GetButtonDown(buttonNames[i])) {
+                if(Cooldowns[i] != 0){
+                    return;
+                }
+                if(InitiateAbilityUse(abilities[i])){
+                    Cooldowns[i] = MaxCooldowns[i];
+                }
+            }
         }
     }
 
-    void InitiateAbilityUse(AbilityType abilityId){
+    bool InitiateAbilityUse(AbilityType abilityId){
         int actorId = gameObject.GetComponent<Actor>().ActorId;
         AbilityInfo abilityInfo = AbilityInfo.InfoArray[(int)abilityId];
         if(abilityInfo.IsArea){
@@ -92,6 +116,7 @@ public class PlayerAbilityController : AbilityController
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (GameObject.Find("Terrain").GetComponent<Collider>().Raycast (ray, out hit, Mathf.Infinity)) {
                 ConnectionManager.Instance.QueueReliableElement(new AreaAbilityElement(actorId, abilityId, hit.point.x, hit.point.z));
+                return true;
             }
         } else if (abilityInfo.IsTargeted){
             RaycastHit hit;
@@ -103,10 +128,12 @@ public class PlayerAbilityController : AbilityController
                 int hitActorId = hitTarget.GetComponent<Actor>().ActorId;
                 //TODO Check if the target is an ally or an enemy and if the ability is allowed to target them
                 ConnectionManager.Instance.QueueReliableElement(new TargetedAbilityElement(actorId, abilityId, hitActorId));
-
+                return true;
             }
         } else if (abilityInfo.IsSelf){
             ConnectionManager.Instance.QueueReliableElement(new TargetedAbilityElement(actorId, abilityId, actorId));
+            return true;
         }
+        return false;
     }
 }
