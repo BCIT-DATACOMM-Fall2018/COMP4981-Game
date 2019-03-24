bc9cc1299ddc9b994e2f0a3d727f5e4c9c76c1fb
Jeffrey Choy <choyjef@gmail.com>
2019-03-23 21:03:23 -0700

    added camera toggle ability; added initial edgepan isometric camera

---
+++ Assets/CameraControllers/Scripts/CameraSelector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSelector : MonoBehaviour
+{
+
+    public Camera playerLockCamera;
+    public Camera isometricCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerLockCamera.enabled = false;
+        isometricCamera.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            playerLockCamera.enabled = !playerLockCamera.enabled;
+            isometricCamera.enabled = !isometricCamera.enabled;
+        }
+        
+    }
+}
---
+++ Assets/CameraControllers/Scripts/IsometricCamera.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class IsometricCamera : MonoBehaviour
+{
+
+    public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+    public Vector2 panLimit;
+    public float mapSize;
+
+    public float scrollSpeed = 20f;
+    public float minY = 20f;
+    public float maxY = 120f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.mousePosition.x <= panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y += scroll * scrollSpeed * 100f * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, 0, mapSize);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        pos.z = Mathf.Clamp(pos.z, 0, mapSize - 100);
+
+        transform.position = pos;
+    }
+}
