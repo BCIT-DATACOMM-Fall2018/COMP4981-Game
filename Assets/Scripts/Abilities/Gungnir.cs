de2a67764efc7098910353f53e5a15e1e413a663
Data Comm <dc@bcit.ca>
2019-04-03 11:51:28 -0700

    Created the 2 abilities without testing

---
+++ Assets/Scripts/Abilities/Gungnir.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gungnir : Ability
+{
+
+    public float speed = 50f;
+    private Vector3 start;
+
+    /// ----------------------------------------------
+    /// FUNCTION:	Start
+    ///
+    /// DATE:		March 14th, 2019
+    ///
+    /// REVISIONS:
+    ///
+    /// DESIGNER:	Cameron Roberts
+    ///
+    /// PROGRAMMER:	Cameron Roberts
+    ///
+    /// INTERFACE: 	void Start()
+    ///
+    /// RETURNS: 	void
+    ///
+    /// NOTES:		MonoBehaviour function.
+    ///             Called before the first Update().
+    /// ----------------------------------------------
+    void Start ()
+    {
+        GetComponent<Rigidbody>().velocity *= speed;
+        start = transform.position;
+    }
+
+    /// ----------------------------------------------
+    /// FUNCTION:	Update
+    ///
+    /// DATE:		March 14th, 2019
+    ///
+    /// REVISIONS:
+    ///
+    /// DESIGNER:	Cameron Roberts
+    ///
+    /// PROGRAMMER:	Cameron Roberts
+    ///
+    /// INTERFACE: 	void Update()
+    ///
+    /// RETURNS: 	void
+    ///
+    /// NOTES:		MonoBehaviour function. Called at a fixed interval.
+    ///             Check how far the GameObject has moved from its
+    ///             starting point and delete it if it has gone too far.
+    /// ----------------------------------------------
+    void Update(){
+        if(Vector3.Distance(start, transform.position) > 500){
+            Destroy(gameObject);
+        }
+    }
+
+    /// ----------------------------------------------
+    /// FUNCTION:	OnTriggerEnter
+    ///
+    /// DATE:		March 14th, 2019
+    ///
+    /// REVISIONS:
+    ///
+    /// DESIGNER:	Cameron Roberts
+    ///
+    /// PROGRAMMER:	Cameron Roberts
+    ///
+    /// INTERFACE: 	void OnCollisionEnter(Collider col)
+    ///
+    /// RETURNS: 	void
+    ///
+    /// NOTES:
+    /// ----------------------------------------------
+    void OnTriggerEnter (Collider col)
+    {
+        if(col.gameObject.tag == creator.tag){
+            Physics.IgnoreCollision(col, GetComponent<Collider>());
+
+        } else{
+            SendCollision(col.gameObject.GetComponent<Actor>().ActorId);
+            //Destroy(gameObject);
+        }
+    }
+}
